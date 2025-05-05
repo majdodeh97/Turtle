318368a1626a86b6b02c6fc8a8acab5e4db2f5f9
Majd Odeh <MajdOdeh@hotmail.com>
2025-05-05 16:37:20 +0300

    adding basic hub downloader script

--- hub/main.lua
+++ hub/main.lua
@@ -1,1 +1,12 @@
 print("Hi, I'm a hub!")
+
+local scriptPath = "downloader.lua"
+
+if fs.exists(scriptPath) then
+    print("Running:", scriptPath)
+    shell.run(scriptPath)
+else
+    print("Error: Script not found at", scriptPath)
+end
+
+os.reboot()
