5443b9368127d402b11ff1c650735422ba053ef2
Majd Odeh <MajdOdeh@hotmail.com>
2025-05-05 16:53:27 +0300

    making hub keep settings

--- hub/main.lua
+++ hub/main.lua
@@ -1,10 +1,12 @@
 print("Hi, I'm a hub!")
 
+sleep(10)
+
 local scriptPath = "downloader.lua"
 
 if fs.exists(scriptPath) then
     print("Running:", scriptPath)
-    shell.run(scriptPath)
+    shell.run(scriptPath, ".settings")
 else
     print("Error: Script not found at", scriptPath)
 end
