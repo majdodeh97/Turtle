09ec85c008cdd591c6cdca5f53f05e8ed0159e28
Majd Odeh <MajdOdeh@hotmail.com>
2025-05-05 14:54:17 +0300

    a

--- clean.lua
+++ clean.lua
@@ -1,7 +1,8 @@
 -- clean.lua
 local keep = {
-    ["download.lua"] = true,
-    ["clean.lua"] = true
+    ["downloader.lua"] = true,
+    ["clean.lua"] = true,
+    ["rom"] = true
 }
 
 for _, file in ipairs(fs.list("")) do
