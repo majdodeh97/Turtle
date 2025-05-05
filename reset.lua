270325943c9be818b0930c8a689c492da2db0cca
Majd Odeh <MajdOdeh@hotmail.com>
2025-05-05 19:23:22 +0300

    Adding resetting only settings

---
+++ reset.lua
@@ -0,0 +1,21 @@
+
+fs.delete(".settings")
+
+local fileName = ".settings"
+local fileURL = "https://raw.githubusercontent.com/majdodeh97/Turtle/main/" .. fileName
+local fileResponse = http.get(fileURL)
+if fileResponse then
+    local content = fileResponse.readAll()
+    fileResponse.close()
+
+    -- Write the file
+    local file = fs.open(fileName, "w")
+    file.write(content)
+    file.close()
+    print("Downloaded:", fileName)
+else
+    error = true
+    print("Failed to download:", line)
+end
+
+os.reboot()
