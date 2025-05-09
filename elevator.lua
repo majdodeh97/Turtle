62aa967f453a097694dd9d6dc2e04897adb531ba
Majd Odeh <MajdOdeh@hotmail.com>
2025-05-09 21:48:41 +0300

    fixing bugs

--- elevator.lua
+++ elevator.lua
@@ -10,12 +10,15 @@ local toMove = destinationZ - locationZ
 local moveFn
 
 if(toMove > 0) then
-    moveFn = move.down
-elseif (toMove < 0) then
     moveFn = move.up
+elseif (toMove < 0) then
+    moveFn = move.down
 else
     print("You are already there")
+    return
 end
+
+toMove = math.abs(toMove)
 
 for i = 1, toMove do
     moveFn()
