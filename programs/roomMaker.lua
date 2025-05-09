a118770d160dee5fcbedf1e8d854d1be23910ab0
Majd Odeh <MajdOdeh@hotmail.com>
2025-05-09 20:25:43 +0300

    roomMaker edits

--- programs/roomMaker.lua
+++ programs/roomMaker.lua
@@ -6,13 +6,14 @@ local inventory = require("/utils/inventory")
 local size = tonumber(arg[1]) or 3
 local height = tonumber(arg[2]) or 2
 local withCeiling = arg[3] ~= "false"
-local itemName = arg[4] or "minecraft:stone_bricks"
+local blockItemName = arg[4] or "minecraft:stone_bricks"
+local slabItemName = arg[5] or "minecraft:stone_brick_slab"
 
-local function buildWallLayer()
+local function buildWallLayer(item)
     for side = 1, 4 do
         for i = 1, size - 1 do
             print(safe.execute(function()
-                return place.itemDown(itemName)
+                return place.itemDown(item)
             end))
             print(safe.execute(move.forward))
         end
@@ -20,35 +21,63 @@ local function buildWallLayer()
     end
 end
 
+local function buildCeiling(item)
+    local leftTurn = false
+
+    for row = 1, size do
+        for col = 1, size do
+            safe.execute(function()
+                return place.itemDown(item)
+            end)
+            if col < size then
+                safe.execute(move.forward)
+            end
+        end
+
+        if row < size then
+            if leftTurn then
+                safe.execute(move.turnLeft)
+                safe.execute(move.forward)
+                safe.execute(move.turnLeft)
+            else
+                safe.execute(move.turnRight)
+                safe.execute(move.forward)
+                safe.execute(move.turnRight)
+            end
+            leftTurn = not leftTurn
+        end
+    end
+    
+    -- return to original position
+
+    if(not leftTurn) then
+        safe.execute(move.turnRight)
+        safe.execute(move.turnRight)
+
+        for i = 1, size - 1 do
+            safe.execute(move.forward)
+        end
+    end
+    
+    safe.execute(move.turnRight)
+
+    for i = 1, size - 1 do
+        safe.execute(move.forward)
+    end
+
+    safe.execute(move.turnRight)
+end
+
 for level = 1, height do
-    buildWallLayer()
+    buildWallLayer(blockItemName)
     safe.execute(move.up)
 end
 
-if(not withCeiling) then return end
-
-local leftTurn = false
-
-for row = 1, size do
-    for col = 1, size do
-        safe.execute(function()
-            return place.itemDown(itemName)
-        end)
-        if col < size then
-            safe.execute(move.forward)
-        end
-    end
-
-    if row < size then
-        if leftTurn then
-            move.turnLeft()
-            safe.execute(move.forward)
-            move.turnLeft()
-        else
-            move.turnRight()
-            safe.execute(move.forward)
-            move.turnRight()
-        end
-        leftTurn = not leftTurn
-    end
+if(not withCeiling) then
+    buildWallLayer(slabItemName)
+else
+    buildCeiling(blockItemName)
+    safe.execute(move.up)
+    buildCeiling(slabItemName)
 end
+
