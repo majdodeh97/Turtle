265be360435eb6486e08baef8b1558a0a30be0aa
Majd Odeh <MajdOdeh@hotmail.com>
2025-05-12 03:41:25 +0300

    fixing reset bug

--- reset.lua
+++ reset.lua
@@ -6,33 +6,33 @@ local fileName = ".settings"
 local fileURL = "https://raw.githubusercontent.com/majdodeh97/Turtle/main/" .. fileName
 local fileResponse = http.get(fileURL)
 
-error = false
+local hasError = false
 
 if fileResponse then
     local content = fileResponse.readAll()
     fileResponse.close()
 
-    -- Write the file
     local file = fs.open(fileName, "w")
     file.write(content)
     file.close()
-    print("Downloaded:", fileName)
+    print("Downloaded: ", fileName)
+
+    settings.load()
+
+    local gpsLocation = navigation.getGpsLocation()
+    if gpsLocation.x and gpsLocation.y and gpsLocation.z then
+        settings.set("location", gpsLocation)
+        settings.save()
+    else
+        hasError = true
+        print("Failed to get GPS location")
+    end
 else
-    error = true
-    print("Failed to download: ", line)
+    hasError = true
+    print("Failed to download: ", fileName)
 end
 
-local gpsLocation = navigation.getGpsLocation()
-if(gpsLocation.x and gpsLocation.y and gpsLocation.z) then
-    settings.set("location", gpsLocation)
-    settings.set("direction", "forward")
-    settings.save()
-else
-    error = true
-    print("Failed to get gps location")
-end
-
-if(not error) then
+if not hasError then
     os.reboot()
 else
     print("Cancelling reboot")
